8d107bc257552a43bc73889aeea2c9d105439f8a
maat <matt@mattchan.xyz>
2023-12-22 17:53:52 -0500

    Return to static generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
+// import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: {
     defaultStrategy: "hover",
   },
-  output: "server",
-  adapter: cloudflare(),
+  output: "static",
+  // adapter: cloudflare(),
 });
