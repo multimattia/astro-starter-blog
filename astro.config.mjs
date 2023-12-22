fc7452c4d6b44a3aaa35335ca52a7a4861cec653
maat <matt@mattchan.xyz>
2023-12-21 22:27:59 -0500

    Add prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: {
+    defaultStrategy: "hover",
+  },
   output: "server",
   adapter: cloudflare(),
 });
