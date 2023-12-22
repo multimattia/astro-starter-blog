9e5da50bc3efcab6d381b91cd1cfe73e08e08c4b
maat <matt@mattchan.xyz>
2023-12-22 17:31:59 -0500

    Install mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: {
-    defaultStrategy: "hover",
+    defaultStrategy: "hover"
   },
   output: "server",
   adapter: cloudflare(),
+  integrations: [mdx()]
 });
