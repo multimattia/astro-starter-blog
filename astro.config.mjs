a4e5f0fe6bb2a8efe705bbd9c37a4f7ef56839e8
maat <matt@mattchan.xyz>
2023-12-22 20:03:10 -0500

    code highlighting test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
-
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: {
-    defaultStrategy: "hover"
+    defaultStrategy: "hover",
   },
   output: "server",
   adapter: cloudflare(),
-  integrations: [mdx()]
+  integrations: [mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: "dracula",
+    },
+  },
 });
