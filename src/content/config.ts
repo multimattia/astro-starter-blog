67cc3f072e575d7a895d9853737d5e9d515c125c
maat <matt@mattchan.xyz>
2024-01-02 12:24:21 -0500

    Add new fiction page, layout, and collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -16,6 +16,22 @@ const postsCollection = defineCollection({
   }),
 });
 
+const storiesCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+  }),
+});
+
 export const collections = {
   posts: postsCollection,
+  fiction: storiesCollection,
 };
