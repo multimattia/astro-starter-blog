c2e6c94e34b3daa1b5b1bbeece46c7c619fc23b8
maat <matt@mattchan.xyz>
2024-01-09 11:33:24 -0500

    Fix broken BlogListing hrefs

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,14 @@
 export interface Post {
-    title: string;
-    pubDate: Date;
-    description: string;
-    author: string;
+    data : {
+     title: string;
+     pubDate: Date;
+     description: string;
+     author: string;
+     tags: string[];
+    }
+    slug: string;
     image: {
      url: string;
      alt: string;
     };
-    tags: string[];
 }
