d9e7083bc2353d1f5ab4c9a57f349ab8ff1039da
maat <matt@mattchan.xyz>
2024-01-23 10:52:13 -0500

    Add fading for load

--- src/components/pizazz/Counter.svelte
+++ src/components/pizazz/Counter.svelte
@@ -1,8 +1,10 @@
 <script>
   import { onMount, onDestroy } from "svelte";
+  import { fade } from "svelte/transition";
   export let blogTitle;
   let isDarkMode = false;
   let count = "loading...";
+  let loading = true;
   // const endpoint = `http://localhost:8787/blog/${slugify(blogTitle)}`;
   const endpoint = `https://blog-api.matthewhongkong20.workers.dev/blog/${slugify(
     blogTitle,
@@ -12,14 +14,15 @@
     return String(str)
       .normalize("NFKD") // split accented characters into their base characters and diacritical marks
       .replace(/[\u0300-\u036f]/g, "") // remove all the accents, which happen to be all in the \u03xx UNICODE block.
-      .trim() // trim leading or trailing whitespace
-      .toLowerCase() // convert to lowercase
+      .trim()
+      .toLowerCase()
       .replace(/[^a-z0-9 -]/g, "") // remove non-alphanumeric characters
       .replace(/\s+/g, "-") // replace spaces with hyphens
       .replace(/-+/g, "-"); // remove consecutive hyphens
   }
 
   onMount(async () => {
+    isDarkMode = window.localStorage.getItem("theme") === "dark";
     const observer = new MutationObserver((mutations) => {
       mutations.forEach((mutation) => {
         if (mutation.attributeName === "class") {
@@ -34,6 +37,7 @@
     if (response.ok) {
       count = await response.json();
       count = parseInt(count);
+      loading = false;
     } else {
       console.error(`Error: ${response.status}`);
     }
@@ -54,32 +58,42 @@
   }
 </script>
 
-<div class={isDarkMode ? "dark" : ""}>
-  <div class="counter">
-    <button on:click={add}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.8"
-        stroke="currentColor"
-        class="likeIcon"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-        />
-      </svg>
-    </button>
-    <span class="likeCounter">
-      {count}
-    </span>
-    <span class="likeCounter"></span>
-  </div>
+<div>
+  {#if !loading}
+    <div class={isDarkMode ? "dark" : ""} transition:fade>
+      <div class="counter">
+        <button on:click={add}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.8"
+            stroke="currentColor"
+            class="likeIcon"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+            />
+          </svg>
+        </button>
+        <span class="likeCounter">
+          {count}
+        </span>
+        <span class="likeCounter"></span>
+      </div>
+    </div>
+  {:else}
+    <div class="loadingBox"></div>
+  {/if}
 </div>
 
 <style>
+  .loadingBox {
+    height: 39px;
+  }
+
   .counter {
     display: flex;
     flex-direction: row;
@@ -115,7 +129,7 @@
     height: 40px;
     width: 40px;
     border: none;
-    border-radius: 50%;
+    border-radius: 5px;
     background-color: var(--color-light-bg);
   }
 
