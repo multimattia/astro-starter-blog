d50948ca72a5801a4c99d44737b8865e4ec41361
maat <matt@mattchan.xyz>
2024-07-11 15:47:40 -0400

    create svelte component to replace server rendering, matching endpoints for each question

---
+++ src/components/pizazz/WnrsQuestion.svelte
@@ -0,0 +1,163 @@
+<script>
+  import { onMount, onDestroy } from "svelte";
+  import { fade, blur } from "svelte/transition";
+  export let level;
+  let isDarkMode = false;
+  let question = "loading...";
+  let loading = true;
+  // const endpoint = `http://localhost:4321/api/wnrs/rand-${level}/`;
+  const endpoint = `https://www.maatwo.com/api/wnrs/rand-${level}/`;
+
+  function slugify(str) {
+    return String(str)
+      .normalize("NFKD") // split accented characters into their base characters and diacritical marks
+      .replace(/[\u0300-\u036f]/g, "") // remove all the accents, which happen to be all in the \u03xx UNICODE block.
+      .trim()
+      .toLowerCase()
+      .replace(/[^a-z0-9 -]/g, "") // remove non-alphanumeric characters
+      .replace(/\s+/g, "-") // replace spaces with hyphens
+      .replace(/-+/g, "-"); // remove consecutive hyphens
+  }
+
+  onMount(async () => {
+    isDarkMode = window.localStorage.getItem("theme") === "dark";
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.attributeName === "class") {
+          isDarkMode = document.documentElement.classList.contains("dark");
+        }
+      });
+    });
+
+    observer.observe(document.documentElement, { attributes: true });
+    try {
+      const response = await fetch(endpoint);
+      if (response.ok) {
+        question = await response.json();
+        // count = parseInt(count);
+        question = question.question;
+        console.log(question);
+        loading = false;
+      } else {
+        console.error(`Error: ${response.status}`);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  });
+
+  async function add() {
+    const res = await fetch(`${endpoint}`, {
+      method: "GET",
+    });
+    try {
+      if (res.ok) {
+        question = await res.json();
+        question = question.question;
+      }
+    } catch (e) {
+      console.error(`Error: ${res.status}`);
+      console.error(`Error: ${e}`);
+    }
+  }
+</script>
+
+<div>
+  {#if !loading}
+    <div class={isDarkMode ? "dark" : ""} transition:fade>
+      <div class="counter">
+        <button on:click={add}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.8"
+            stroke="currentColor"
+            class="questionIcon"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
+            />
+          </svg>
+        </button>
+        <span class="likeCounter" transition:blur>
+          {#key question}
+            <p in:fade>{question}</p>
+          {/key}
+        </span>
+        <span class="likeCounter"></span>
+      </div>
+    </div>
+  {:else}
+    <div class="loadingBox"></div>
+  {/if}
+</div>
+
+<style>
+  .loadingBox {
+    height: 40px;
+  }
+
+  .counter {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    min-width: 8ch;
+    border-radius: 7px;
+  }
+
+  .questionIcon {
+    width: 30px;
+    height: 30px;
+    color: var(--color-light-a);
+  }
+
+  .dark .questionIcon {
+    color: var(--color-night-a);
+  }
+
+  .likeCounter {
+    margin-left: 0.3ch;
+    margin-right: 10px;
+    font-size: 1.1rem;
+    color: var(--color-light-deemp);
+  }
+
+  .dark .likeCounter {
+    color: var(--color-night-deemp);
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    height: 40px;
+    width: 40px;
+    border: none;
+    border-radius: 5px;
+    background-color: var(--color-light-bg);
+  }
+
+  .dark button {
+    background-color: var(--color-night-bg);
+  }
+
+  button:hover {
+    background-color: var(--color-light-a-highlight);
+    transition: background-color 0.2s var(--improved-ease);
+  }
+
+  .dark button:hover {
+    background-color: var(--color-night-a-highlight);
+  }
+
+  button:active {
+    transform: translateY(3px);
+    transition: transform 0.17s ease-out;
+  }
+</style>
---
+++ src/pages/api/wnrs/rand-1.ts
@@ -0,0 +1,41 @@
+export async function GET({ params, request }) {
+  const questions = [
+    "What do you think my major is?",
+    "Do you think I've ever been in love?",
+    "Do you think I've ever had my heart broken?",
+    "Do you think I've ever been fired?",
+    "Do you think I was popular in high school?",
+    "What do you think I will prefer? Hot Cheetos or onion rings?",
+    "Do you think I like being a couch potato?",
+    "Do you think I am an extrovert?",
+    "Do you think I have a sibling? Older or younger?",
+    "Where do you think I grew up?",
+    "Do you think I am mainly cooking or getting takeout?",
+    "What do you think I have been binge-watching lately?",
+    "Do you think I hate waking up early?",
+    "What is the nicest thing you can remember doing for a friend?",
+    "What type of social situation makes you feel the most awkward?",
+    "Who do you think is my favorite idol?",
+    "When do I usually have dinner?",
+    "Do you think I like wearing red?",
+    "What do you think is my favorite dish?",
+    "Do you think I'm in Greek life?",
+    "Do you know what my dream career is?",
+    "Do you know where my dream vacation is?",
+    "Do you think I used to be bullied in school?",
+    "Do you think I am a talkative person?",
+    "Do you think I am a cold fish?",
+    "What do you think my favorite Starbucks drink is?",
+    "Do you think I love reading books?",
+    "When do you think I most usually like to stay alone?",
+    "Which part of a house do you think is my favorite place?",
+    "Do you think I like playing video games?",
+  ];
+  const randomQuestion =
+    questions[Math.floor(Math.random() * questions.length)];
+  return new Response(
+    JSON.stringify({
+      question: randomQuestion,
+    }),
+  );
+}
